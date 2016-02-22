713410e0986715d8ffd94665327afe5fd2b2a7ac
Pedro Henrique <Pedro Henrique>
2016-02-21 21:24:44 -0300

    Site recriado

---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+var images = {};
+
+function init_index() {
+	fill_carroussel();
+}
+
+function fill_news() {
+	html = ""
+	for (var i = 0; i < images.length; i++) {
+		images[i]
+	}
+}
+
+/*
+<tr>
+	<td style="width: 40%;">
+		<div class="container-fluid">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="carousel slide" id="carousel-215943">
+							<ol class="carousel-indicators">
+								<li class="active" data-slide-to="0" data-target="#carousel-215943">
+								</li>
+								<li data-slide-to="1" data-target="#carousel-215943">
+								</li>
+								<li data-slide-to="2" data-target="#carousel-215943">
+								</li>
+							</ol>
+							<div class="carousel-inner">
+								<div class="item active">
+									<img style="border-radius: 25px;" alt="Carousel Bootstrap First" src="img/car/deadpool_0.jpg" />
+								</div>
+								<div class="item">
+									<img style="border-radius: 25px;" alt="Carousel Bootstrap Second" src="img/car/deadpool_1.jpg" />
+								</div>
+								<div class="item">
+									<img style="border-radius: 25px;" alt="Carousel Bootstrap Third" src="img/car/deadpool_2.jpg" />
+								</div>
+							</div> 
+							<a style="border-radius: 25px;" class="left carousel-control" href="#carousel-215943" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left"></span>
+							</a>
+							<a style="border-radius: 25px;" class="right carousel-control" href="#carousel-215943" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right"></span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+	</td>
+	<td style="width: 60%;">
+		<div style="border-radius: 25px; background-color: #EEEEEE; padding: 10px;">
+			<h2 style="margin-top: 0px;">Deadpool (Filme)</h2>
+			<p>Sinopse: Ex-militar e mercenário, Wade Wilson (Ryan Reynolds) é diagnosticado com câncer em estado terminal, porém encontra uma possibilidade de cura em uma sinistra experiência científica. Recuperado, com poderes e um incomum senso de humor, ele torna-se Deadpool e busca vingança contra o homem que destruiu sua vida.</p>
+			<a target="_blank" href="http://www.adorocinema.com/filmes/filme-146349/">Mais detalhes sobre o filme!</a>
+		</div>
+	</td>
+</tr>
+*/
